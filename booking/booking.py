ec93e0255cdb166115a75923918868a6c4f85911
mrawal <imsr1013@gmail.com>
2022-08-05 15:33:50 +0545

    changing currency in bookings page

--- booking/booking.py
+++ booking/booking.py
@@ -10,6 +10,8 @@ class Booking(webdriver.Chrome):
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+        self.implicitly_wait(55)
+        self.maximize_window()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
@@ -17,4 +19,14 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def change_currency(self,currency=None):
+        currency_element=self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
+        selected_currency_element = self.find_element_by_css_selector(
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
+        selected_currency_element.click()
 
--- run.py
+++ run.py
@@ -5,3 +5,4 @@ from booking.booking import Booking
 
 with Booking(teardown=True) as bot:
     bot.land_first_page()
+    bot.change_currency(currency='GBP')
