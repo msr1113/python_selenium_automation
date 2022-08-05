3dcf8bcd1d5bad2411731db71fa99d769fbaad2c
mrawal <imsr1013@gmail.com>
2022-08-05 16:49:15 +0545

    select check in and check out date after searching location

--- run.py
+++ run.py
@@ -5,4 +5,6 @@ from booking.booking import Booking
 
 with Booking(teardown=True) as bot:
     bot.land_first_page()
-    bot.change_currency(currency='GBP')
+    # bot.change_currency('GBP')
+    bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date="2022-08-05",check_out_date="2022-08-17")
